cb0865ca9c0747ff8316ae75afdaf8c9084242f6
chanzm <37019789+chanzm@users.noreply.github.com>
2019-04-05 09:52:39 +0700

    Done no1

--- shift1.c
+++ shift1.c
@@ -2,44 +2,66 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int faktorial(int x);
-//pthread_t *thread;//inisialisasi awal
-//int t;
+void *faktorial( void *arr );
+void swap(int *xp, int *yp);
 
+pthread_t *thread;//inisialisasi awal
+int arr[1000];
+int banyak=0;
 int main()
 {
-	int i=0;
-	int t=0;
-	char masuk[1000];
-	int arr[1000];
-	while(1)
-	{
-		char chr;
-		scanf("%d%c", &arr[t], &chr);
-		t++;
-		int  j, min_idx;
+        int i=0;
+        int t=0;
+        int iret1;
+        while(1)
+        {
+                char chr;
+                scanf("%d%c", &arr[t], &chr);
+                thread=malloc(t*sizeof(int));
+                t++;
+                int  j, min_idx;
 
-    			//One by one move boundary of unsorted subarray 
-    		for (i=0; i<t; i++) 
-    		{
-        		// Find the minimum element in unsorted array 
-      			min_idx = i;
-        		for (j=i+1; j<t; j++)
-          			if (arr[j] < arr[min_idx]) min_idx = j;
-        				//Swap the found minimum element with the first element
-        		swap(&arr[min_idx], &arr[i]);
-		}
+                        //One by one move boundary of unsorted subarray 
+                for (i=0; i<t; i++) 
+                {
+                        // Find the minimum element in unsorted array 
+                        min_idx = i;
+                        for (j=i+1; j<t; j++)
+                                if (arr[j] < arr[min_idx]) min_idx = j;
+                                        //Swap the found minimum element with the first element
+                        swap(&arr[min_idx], &arr[i]);
+                }
 
-		if(chr == '\n') break;
-	}
-	for(i=0;i<t;i++) printf("%d! = %d \n", arr[i], faktorial(arr[i]));
+                if(chr == '\n') break;
+        }
+        for(i=0;i<t;i++)
+        {
+                iret1 = pthread_create( &thread[banyak], NULL, faktorial, NULL);//membuat thread pertama
+                if(iret1)//jika eror
+                {
+                        fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
+                        exit(EXIT_FAILURE);
+                }
+                pthread_join( thread[i], NULL); 
+                 banyak++;
+                if(t==banyak) break;
+         }
+        // for(int i=0;i<banyak;i++) pthread_join( thread[i], NULL); 
+
+        exit(EXIT_SUCCESS);
+
 }
 
-int faktorial(int x)
+
+void *faktorial( void *ar )
 {
-	if(x<2) return 1;
-	else return x*faktorial(x-1);
+    int x=arr[banyak];
+    int total=1;
+    int c;
+    for(c=x;c>0;c--) total*=c;
+    printf("%d! = %d\n", x,total);
 }
+
 
 void swap(int *xp, int *yp)
 {
